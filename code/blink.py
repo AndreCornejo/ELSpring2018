8c2931376619be27bf4d7736f7d8c9f8f50112ca
AndreCornejo <cornejoa1@hawkmail.newpaltz.edu>
2019-02-21 18:57:32 -0500

    Added the python files for the blinky light and the temperature sensor thingy

---
+++ code/blink.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+import time
+import os
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+def blinkOnce(pin):
+	GPIO.output(pin,True)
+	time.sleep(.1)
+	GPIO.output(pin,False)
+	time.sleep(.1)
+
+try:
+	while True:
+		input_state = GPIO.input(26)
+		if input_state == False:
+			for i in range(10):
+				blinkOnce(17)
+			time.sleep(.2)
+
+except KeyboardInterrupt:
+	os.system('clear')
+	print('Thanks for Blinking and Thinking!')
+	GPIO.cleanup()
